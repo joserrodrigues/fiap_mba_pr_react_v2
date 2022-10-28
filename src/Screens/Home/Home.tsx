eaf0c2dc92f8c8d711ae79f2a8f7030412a83bb3
rubens Rodrigues <rubens@schoolguardian.app>
2022-10-28 00:44:27 -0300

    Class 1 - Section 4 - React Hooks / useState

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import "./Home.css";
 import CustomLabel from "../../Components/CustomLabel/CustomLabel";
 
 function App() {
+  const [count, setCount] = useState<number>(0);
+
+    const addCount = () => {
+      setCount((count) => count + 1);
+    };
+    
   return (
     <div className="App">
-      <CustomLabel info={"Teste"} />
+      <CustomLabel info={"Contador = " + count} />
+      <button onClick={() => addCount()}> Add Info</button>
     </div>
   );
 }
