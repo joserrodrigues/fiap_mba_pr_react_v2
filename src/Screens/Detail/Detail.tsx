efb6ca5b031842da8edb1e573d04c17b9f062d7b
rubens Rodrigues <rubens@schoolguardian.app>
2022-10-29 14:53:27 -0300

    Class 3 - Section 2 - Maps

--- src/Screens/Detail/Detail.tsx
+++ src/Screens/Detail/Detail.tsx
@@ -1,9 +1,24 @@
 import React from "react";
-import { NavigateFunction, useNavigate, useParams } from "react-router-dom";
-import { ButtonMaxSize, MainStack, TitlePage } from "./DetailStyles";
+import { NavigateFunction, useNavigate, useParams, useLocation } from "react-router-dom";
+import { ButtonMaxSize, MainStack, TitlePage, StyledMap } from "./DetailStyles";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { LatLngExpression } from "leaflet";
+
+type LocationProps = {
+  lat: number;
+  lng: number;
+};
 
 const Detail = () => {
   const { infoID } = useParams();
+  let markerPosition: LatLngExpression = [0, 0];
+  const location = useLocation();
+  if (location.state) {
+    let infoLocation: LocationProps = location.state as LocationProps;
+    console.log(infoLocation);
+    markerPosition = [infoLocation.lat, infoLocation.lng];
+  }
+  
   let navigate: NavigateFunction = useNavigate();
     const onBackButton = () => {
     navigate(-1);
@@ -19,6 +34,24 @@ const Detail = () => {
           Voltar
         </ButtonMaxSize>
       </MainStack>
+      <StyledMap>
+        <MapContainer
+          center={markerPosition}
+          zoom={13}
+          scrollWheelZoom={false}
+          style={{ height: "100%" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={markerPosition}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </StyledMap>
     </>
   );
 };
--- src/Screens/Detail/DetailStyles.tsx
+++ src/Screens/Detail/DetailStyles.tsx
@@ -5,7 +5,7 @@ import Stack from "@mui/material/Stack";
 
 export const MainStack = styled(Stack)`
   && {
-    margin-top: 150px;
+    margin-top: 80px;
     text-align: center;
     width: 100%;
     align-items: center;
@@ -21,4 +21,10 @@ export const ButtonMaxSize = styled(Button)`
   && {
     max-width: 200px;
   }
+`;
+
+export const StyledMap = styled.div`
+  margin-top: 40px;
+  width: "100%";
+  height: 400px;
 `;
